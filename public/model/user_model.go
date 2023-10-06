1eb1ec39ac321936ec1368da0ef957d1e0f7162c
JonathanDowdell <jdowde2@wgu.edu>
2023-10-05 21:36:28 -0500

    Updated Proto

--- public/model/user_model.go
+++ public/model/user_model.go
@@ -3,6 +3,7 @@ package model
 import (
 	"time"
 
+	"github.com/Bit-Bridge-Source/BitBridge-UserService-Go/proto/pb"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -16,4 +17,12 @@ type CreateUserModel struct {
 	Email    string `json:"email" bson:"email"`
 	Username string `json:"username" bson:"username"`
 	Password string `json:"password" bson:"password"`
+}
+
+func (c *CreateUserModel) ToCreateUserRequest() *pb.CreateUserRequest {
+	return &pb.CreateUserRequest{
+		Email:    c.Email,
+		Username: c.Username,
+		Password: c.Password,
+	}
 }
